f220a25d59a710084dec764d05799cdd114066bd
Yuvika <cyuvika910@gmail.com>
2025-11-16 16:19:28 +0530

    microservice added

--- notification-service/server.js
+++ notification-service/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const subscriber = require('./queue'); // <-- importing queue.js
+const subscriber = require('./queue');
 
-const app = express();
+const app = express();  
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
