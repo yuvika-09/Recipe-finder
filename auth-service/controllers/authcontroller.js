4fd88473ba1caef04a6f621874c632a5d9d11ddf
Samiya30 <156989413+Samiya30@users.noreply.github.com>
2025-11-16 18:59:56 +0530

    Update authcontroller.js

--- auth-service/controllers/authcontroller.js
+++ auth-service/controllers/authcontroller.js
@@ -16,7 +16,6 @@ exports.register = async (req, res) => {
     res.json({ message: 'User registered successfully' });
 };
 
-
 exports.login = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username, password });
